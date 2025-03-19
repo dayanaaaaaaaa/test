02e08f4376f188fee8503f09c76062e59b9f7767
dayanaaaaaaaa <dayanaisidorag@gmail.com>
2025-03-19 14:30:11 +0200

    wednesday

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def index():
 @app.route("/sveiki")
 def sveiki():
     vards = ["Marta","Anna", "Katrina"]
-    return render_template("sveiki.html", name=vards, last="")
+    return render_template("sveiki.html", name=vards, last=" ")
 
 @app.route("/sveiki/<vards1>/<uzvards1>")
 def sveiki_cits(vards1, uzvards1):
-    vards = vards1
+    vards = [vards1]
     uzvards = uzvards1 
     return render_template("sveiki.html", name=vards, last=uzvards)
 
