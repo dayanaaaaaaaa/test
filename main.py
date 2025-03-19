03253a5268f9ba7a50e87748f18b5a6fb8302886
dayanaaaaaaaa <dayanaisidorag@gmail.com>
2025-03-19 14:25:24 +0200

    today

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():      
+def index():
     return render_template("test.html")
 
-if __name__== '__main__':
-    app.run(port=5000) 
+@app.route("/sveiki")
+def sveiki():
+    vards = ["Marta","Anna", "Katrina"]
+    return render_template("sveiki.html", name=vards, last="")
+
+@app.route("/sveiki/<vards1>/<uzvards1>")
+def sveiki_cits(vards1, uzvards1):
+    vards = vards1
+    uzvards = uzvards1 
+    return render_template("sveiki.html", name=vards, last=uzvards)
+
+if __name__ == '__main__':
+    app.run(port=5000)
